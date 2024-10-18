0c1a561210107b7b512f7affe7ec6c3a9ab0b37a
moucheh <moucheh@proton.me>
2024-10-19 00:04:38 +0200

    .

--- av/v1/p1.fsx
+++ av/v1/p1.fsx
@@ -2,5 +2,6 @@
 // za integere (a,b,c) -> (26, 28, 18). Nakon toga koristi tri
 // simbola da izracuna zajednicke bite integera koristeci
 // AND operator.
+
 let a, b, c = (26, 28, 18)
 printfn "%d" (a &&& b &&& c)
