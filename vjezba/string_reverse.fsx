94fd08888f15371f18ef229f26881c0b4fbf6412
moucheh <moucheh@proton.me>
2024-11-28 22:16:42 +0100

    string reverse

--- vjezba/string_reverse.fsx
+++ vjezba/string_reverse.fsx
@@ -2,10 +2,10 @@ let a = "Hello World!"
 let rec reverse (str : string) : string =
   match str.Length with
   | 0 -> ""
-  | 1 -> (str[0]).ToString ()
+  | 1 -> str[0] |> string
   | _ -> 
     let last = str.Length - 1 
     let new_bound = last - 1
-    (str[last]).ToString () + reverse str[0..new_bound]
+    string str[last] + reverse str[0..new_bound]
 
 a |> reverse |> printfn "%s"
